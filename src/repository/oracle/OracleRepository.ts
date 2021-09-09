a2f89ce19ded340779f166b34174634dab792b01
朱博功 <61008596@qq.com>
2021-09-09 18:58:28 +0800

    add oracle crud

--- src/repository/oracle/OracleRepository.ts
+++ src/repository/oracle/OracleRepository.ts
@@ -1,12 +1,24 @@
 import {
     GetPageParams,
     GetPageParamsSchema,
+    GetsByFilterParams,
+    GetsByFilterParamsSchema,
+    UpdatesByFilterParams,
+    UpdatesByFilterParamsSchema,
+    SaveByFilterParams,
+    SaveByFilterParamsSchema,
+    DeletesByFilterParams,
+    DeletesByFilterParamsSchema,
+    executeProcedureParams,
+    executeProcedureParamsSchema,
     ExecuteSqlParams,
     ExecuteSqlParamsSchema,
 } from '../types/RepositoryParams';
 import { JoiUtils } from '../../util/JoiUtils';
 import { findTableName } from "../../util/globals";
-
+import { knex } from 'knex';
+const oracledb = require('oracledb');
+const _ = require('lodash');
 /**
  * Entity manager supposed to work with any entity, automatically find its repository and call its methods,
  * whatever entity type are you passing.
@@ -21,6 +33,8 @@ export class OracleRepository {
      * Connection used by this entity manager.
      */
     connection: any;
+
+    queryBuilder: any;
 
     entity: any;
 
@@ -37,6 +51,7 @@ export class OracleRepository {
                 throw Error(`constructor params::_entity can not be ${JSON.stringify(_entity)} !!!`);
             }
             this.connection = _connection;
+            this.queryBuilder = knex({ client: 'oracledb' });
             this.entity = _entity;
             this.tableName = findTableName(_entity);
         } catch (err) {
@@ -82,13 +97,20 @@ export class OracleRepository {
     async getPage(params: GetPageParams) {
         try {
             await JoiUtils.checkParams(GetPageParamsSchema, params);
-            let { current, pageSize } = params;
-            let sql: string;
+            let { current, pageSize, options } = params;
+            let sql = '';
+            let count_sql = '';
             //todo add sql builder and make sure entity.name is not undefined
-
-            sql = `SELECT * FROM (SELECT A.*, ROWNUM AS MY_RNUM FROM ( SELECT * FROM ${this.tableName}) A
+            if (!!options && !!options.schema) {
+                sql = `SELECT * FROM (SELECT A.*, ROWNUM AS MY_RNUM FROM ( SELECT * FROM "${options.schema}"."${this.tableName}") A
                 WHERE ROWNUM <= :maxnumrows + :offset) WHERE MY_RNUM > :offset`
-            let count_sql = `SELECT count(id) FROM ${this.tableName}`
+                count_sql = `SELECT count(id) FROM "${options.schema}"."${this.tableName}"`
+            } else {
+                sql = `SELECT * FROM (SELECT A.*, ROWNUM AS MY_RNUM FROM ( SELECT * FROM "${this.tableName}") A
+                WHERE ROWNUM <= :maxnumrows + :offset) WHERE MY_RNUM > :offset`
+                count_sql = `SELECT count(id) FROM "${this.tableName}"`
+            }
+
             const result1 = await this.executeSql({
                 sql,
                 binds: { offset: (current - 1) * pageSize, maxnumrows: pageSize },
@@ -112,5 +134,360 @@ export class OracleRepository {
         }
     }
 
+    /**
+    * get documents by filter of collection
+    * @param {object} _params - params object
+    * @param {object} _params.filter - filter of attrs.
+    * @param {object} _params.like - the object to like the documents to get from the collection
+    * @param {array} _params.sorts - array of sort str for attrs.
+    * @param {object} _params.options - query options, e.g. keepAttrs-res attrs; hasEdge-edge res;
+    * @return {AqlQuery} - interface AqlQuery by arangodb
+    */
+    async getsByFilter(params: GetsByFilterParams) {
+        try {
+            await JoiUtils.checkParams(GetsByFilterParamsSchema, params);
+            let { filter, options } = params;
+            let filterTemp = Object.assign({}, filter);
+            // let { filter, sorts, options } = params;
+            let tempQuery = this.queryBuilder(this.tableName);
+            for (const key in filterTemp) {
+                const data = filterTemp[key];
+                if (Array.isArray(data)) {
+                    // 数组参数，则‘in’
+                    tempQuery = tempQuery.whereIn(key, data);
+                    delete filterTemp[key];
+                } else if (!!data && typeof data === 'object') {
+                    //todo 暂时不支持
+                    /**
+                    * 对象参数结构{opr, value}
+                    * 支持：==, !=, <, <=, >, >=, IN, NOT IN, LIKE, =~, !~
+                    * 注意: typeof null === 'object'
+                    * 增加：opr = 'POSITION' 时需要处理数组属性的查询参数
+                    */
+                    delete filterTemp[key];
+                }
+            }
+
+            if (Object.keys(filterTemp).length !== 0) {
+                tempQuery = tempQuery.where({
+                    ...filterTemp
+                });
+            }
+
+            if (!!options && !!options.schema) {
+                tempQuery = tempQuery.withSchema(options.schema)
+            }
+
+            if (!!options && !!options.keepAttrs) {
+                tempQuery = tempQuery.column(options.keepAttrs).select()
+            } else {
+                tempQuery = tempQuery.select('*')
+            }
+
+            const query = tempQuery.toSQL().toNative()
+            console.log("getsByFilter query: ", query)
+            const result = await this.executeSql({
+                sql: query.sql,
+                binds: query.bindings,
+            });
+
+            return result.rows;
+
+        } catch (err) {
+            console.error("OracleRepository getsByFilter 失败!!!", err);
+            throw Error(`OracleRepository getsByFilter 失败!!!, 错误: ${err}`)
+        }
+    }
+
+    /**
+    * update document by _id && newObj from collection
+    * @param {object} _params - the _id of the document
+    * @param {string} _params.filter - the _id of the document
+    * @param {object} _params.newObj - the newObj of the document
+    * @return {AqlQuery} - interface AqlQuery by arangodb
+    */
+    async updatesByFilter(params: UpdatesByFilterParams) {
+        try {
+            await JoiUtils.checkParams(UpdatesByFilterParamsSchema, params);
+            let { filter, newObj, options } = params;
+            let filterTemp = Object.assign({}, filter);
+            //todo 检查Entity attributes
+            let newObjTemp = Object.assign({}, newObj);
+            // let { filter, sorts, options } = params;
+            let tempQuery = this.queryBuilder(this.tableName);
+            for (const key in filterTemp) {
+                const data = filterTemp[key];
+                if (Array.isArray(data)) {
+                    // 数组参数，则‘in’
+                    tempQuery = tempQuery.whereIn(key, data);
+                    delete filterTemp[key];
+                } else if (!!data && typeof data === 'object') {
+                    //todo 暂时不支持
+                    /**
+                    * 对象参数结构{opr, value}
+                    * 支持：==, !=, <, <=, >, >=, IN, NOT IN, LIKE, =~, !~
+                    * 注意: typeof null === 'object'
+                    * 增加：opr = 'POSITION' 时需要处理数组属性的查询参数
+                    */
+                    delete filterTemp[key];
+                }
+            }
+
+            if (Object.keys(filterTemp).length !== 0) {
+                tempQuery = tempQuery.where({
+                    ...filterTemp
+                });
+            }
+
+            if (!!options && !!options.schema) {
+                tempQuery = tempQuery.withSchema(options.schema)
+            }
+
+            if (!!options && !!options.returns) {
+                tempQuery = tempQuery.returning(options.returns)
+            }
+
+            const query = tempQuery.update({
+                ...newObjTemp
+            }).toSQL().toNative();
+
+            let bindingsObj = {};
+
+            for (let i = 0; i < query.bindings.length; i++) {
+
+                if (typeof query.bindings[i] === 'object' && query.bindings[i].constructor.name === 'ReturningHelper') {
+                    // bindingsObj[i] = {
+                    //     [query.bindings[i].columnName]: { dir: oracledb.BIND_OUT }
+                    // };
+                    query.sql = _.replace(query.sql, `:${i + 1}`, `:${query.bindings[i].columnName}`);
+                    bindingsObj[query.bindings[i].columnName + ''] = { dir: oracledb.BIND_OUT };
+                } else {
+                    bindingsObj[i + 1] = query.bindings[i];
+                }
+            }
+
+            // console.log('bindingsObj: ', bindingsObj);
+
+            console.log("updatesByFilter query: ", query)
+            const result = await this.executeSql({
+                sql: query.sql,
+                binds: bindingsObj,
+                options: { autoCommit: true }
+            });
+
+            return result.rows;
+
+        } catch (err) {
+            console.error("OracleRepository updatesByFilter 失败!!!", err);
+            throw Error(`OracleRepository updatesByFilter 失败!!!, 错误: ${err}`)
+        }
+    }
+
+    async save(params: SaveByFilterParams) {
+        try {
+            await JoiUtils.checkParams(SaveByFilterParamsSchema, params);
+            let { obj, options } = params;
+            //todo 检查Entity attributes
+            let tempQuery = this.queryBuilder(this.tableName);
+
+            if (!!options && !!options.schema) {
+                tempQuery = tempQuery.withSchema(options.schema)
+            }
+
+            if (!!options && !!options.returns) {
+                tempQuery = tempQuery.returning(options.returns)
+            }
+
+            const query = tempQuery.insert(obj).toSQL().toNative();
+
+            let bindingsObj = {};
+
+            for (let i = 0; i < query.bindings.length; i++) {
+
+                if (typeof query.bindings[i] === 'object' && query.bindings[i].constructor.name === 'ReturningHelper') {
+                    // bindingsObj[i] = {
+                    //     [query.bindings[i].columnName]: { dir: oracledb.BIND_OUT }
+                    // };
+                    query.sql = _.replace(query.sql, `:${i + 1}`, `:${query.bindings[i].columnName}`);
+                    bindingsObj[query.bindings[i].columnName + ''] = { dir: oracledb.BIND_OUT };
+                } else {
+                    bindingsObj[i + 1] = query.bindings[i];
+                }
+            }
+
+            // console.log('bindingsObj: ', bindingsObj);
+
+            // if (!!options && !!options.returns && Array.isArray(options.returns)) {
+            //     // if(query.bindings[i])
+            //     let returningStr = ' returning';
+            //     for (let i = 0; i < options.returns.length; i++) {
+            //         returningStr = returningStr + ` "${options.returns[i]}" INTO :${options.returns[i]}`
+            //     }
+            //     query.sql = query.sql + returningStr;
+            // }
+
+            const result = await this.executeSql({
+                sql: query.sql,
+                binds: bindingsObj,
+                options: { autoCommit: true }
+            });
+
+            return result.rows;
+
+        } catch (err) {
+            console.error("OracleRepository save 失败!!!", err);
+            throw Error(`OracleRepository save 失败!!!, 错误: ${err}`)
+        }
+    }
+
+    async deletesByFilter(params: DeletesByFilterParams) {
+        try {
+            await JoiUtils.checkParams(DeletesByFilterParamsSchema, params);
+            let { filter, options } = params;
+            let filterTemp = Object.assign({}, filter);
+            //todo 检查Entity attributes
+            // let { filter, sorts, options } = params;
+            let tempQuery = this.queryBuilder(this.tableName);
+            for (const key in filterTemp) {
+                const data = filterTemp[key];
+                if (Array.isArray(data)) {
+                    // 数组参数，则‘in’
+                    tempQuery = tempQuery.whereIn(key, data);
+                    delete filterTemp[key];
+                } else if (!!data && typeof data === 'object') {
+                    //todo 暂时不支持
+                    /**
+                    * 对象参数结构{opr, value}
+                    * 支持：==, !=, <, <=, >, >=, IN, NOT IN, LIKE, =~, !~
+                    * 注意: typeof null === 'object'
+                    * 增加：opr = 'POSITION' 时需要处理数组属性的查询参数
+                    */
+                    delete filterTemp[key];
+                }
+            }
+
+            if (Object.keys(filterTemp).length !== 0) {
+                tempQuery = tempQuery.where({
+                    ...filterTemp
+                });
+            }
+
+            if (!!options && !!options.schema) {
+                tempQuery = tempQuery.withSchema(options.schema)
+            }
+
+            //todo return can't work
+            // if (!!options && !!options.returns) {
+            //     tempQuery = tempQuery.returning(options.returns)
+            // }
+
+            const query = tempQuery.del().toSQL().toNative();
+
+            let bindingsObj = {};
+
+            for (let i = 0; i < query.bindings.length; i++) {
+
+                if (typeof query.bindings[i] === 'object' && query.bindings[i].constructor.name === 'ReturningHelper') {
+
+                    query.sql = _.replace(query.sql, `:${i + 1}`, `:${query.bindings[i].columnName}`);
+                    bindingsObj[query.bindings[i].columnName + ''] = { dir: oracledb.BIND_OUT };
+                } else {
+                    bindingsObj[i + 1] = query.bindings[i];
+                }
+            }
+
+            if (!!options && !!options.returns && Array.isArray(options.returns)) {
+                let returningStr = ' returning';
+                for (let i = 0; i < options.returns.length; i++) {
+                    returningStr = returningStr + ` "${options.returns[i]}" INTO :${options.returns[i]}`
+                    bindingsObj[options.returns[i] + ''] = { dir: oracledb.BIND_OUT };
+                }
+                query.sql = query.sql + returningStr;
+            }
+
+            // console.log('bindingsObj: ', bindingsObj);
+            const result = await this.executeSql({
+                sql: query.sql,
+                binds: bindingsObj,
+                options: { autoCommit: true }
+            });
+
+            return result.rows;
+
+        } catch (err) {
+            console.error("OracleRepository deletesByFilter 失败!!!", err);
+            throw Error(`OracleRepository deletesByFilter 失败!!!, 错误: ${err}`)
+        }
+    }
+
+    async executeProcedure(params: executeProcedureParams) {
+        try {
+            await JoiUtils.checkParams(executeProcedureParamsSchema, params);
+            // let { filter, options } = params;
+            // let filterTemp = Object.assign({}, filter);
+            //todo 检查Entity attributes
+            // let { filter, sorts, options } = params;
+            // let tempQuery = this.queryBuilder(this.tableName);
+            // for (const key in filterTemp) {
+            //     const data = filterTemp[key];
+            //     if (Array.isArray(data)) {
+            //         // 数组参数，则‘in’
+            //         tempQuery = tempQuery.whereIn(key, data);
+            //         delete filterTemp[key];
+            //     } else if (!!data && typeof data === 'object') {
+            //         //todo 暂时不支持
+            //         /**
+            //         * 对象参数结构{opr, value}
+            //         * 支持：==, !=, <, <=, >, >=, IN, NOT IN, LIKE, =~, !~
+            //         * 注意: typeof null === 'object'
+            //         * 增加：opr = 'POSITION' 时需要处理数组属性的查询参数
+            //         */
+            //         delete filterTemp[key];
+            //     }
+            // }
+
+            // if (Object.keys(filterTemp).length !== 0) {
+            //     tempQuery = tempQuery.where({
+            //         ...filterTemp
+            //     });
+            // }
+            // var schema = '';
+            // if (!!options && !!options.schema) {
+            //     schema = options.schema;
+            // }
+
+            //todo return can't work
+            // if (!!options && !!options.returns) {
+            //     tempQuery = tempQuery.returning(options.returns)
+            // }
+
+
+            const query = this.queryBuilder.raw(
+                `BEGIN
+                    CMSDTP.ZDSYJ_SCDD(:AS_INPTSTR, :AS_ISEXESQL, :AS_EXESQL, :AI_RTD, :AS_RMSG);
+                    COMMIT;
+                END;`
+            ).toSQL().toNative();
+
+            console.log('query: ', query);
+            const result = await this.executeSql({
+                sql: query.sql,
+                binds: {
+                    AS_INPTSTR: params.json,
+                    AS_ISEXESQL: { dir: oracledb.BIND_OUT, type: oracledb.STRING, maxSize: 2000 },
+                    AS_EXESQL: { dir: oracledb.BIND_OUT, type: oracledb.CLOB },
+                    AI_RTD: { dir: oracledb.BIND_OUT, type: oracledb.NUMBER },
+                    AS_RMSG: { dir: oracledb.BIND_OUT, type: oracledb.STRING, maxSize: 2000 },
+                },
+                // options: { autoCommit: true }
+            });
+
+            return result.rows;
+
+        } catch (err) {
+            console.error("OracleRepository executeProcedure 失败!!!", err);
+            throw Error(`OracleRepository executeProcedure 失败!!!, 错误: ${err}`)
+        }
+    }
 
 }
--- src/repository/types/RepositoryParams.ts
+++ src/repository/types/RepositoryParams.ts
@@ -1,24 +1,97 @@
+// import { array } from 'joi';
 import Joi = require('joi');
+
+// 最少一个不为非法不可空（保存/查询单个）：限制至少一个属性，不为空（'', null），不允许非法字符
+const filterMinRequired = Joi.object().min(1).pattern(/.*/, Joi.any().invalid('', null, NaN, Infinity).required());
 
 export interface GetPageParams {
     current: number;
     pageSize: number;
+    options: Options;
 }
 
 export let GetPageParamsSchema = Joi.object({
     current: Joi.number().strict().integer().min(1).required(),
     pageSize: Joi.number().strict().integer().min(10).required(),
+    options: Joi.object(),
+}).required();
+
+
+export interface Options {
+    keepAttrs: Array<string>;
+    schema: string;
+    returns: Array<string>;
+}
+
+export interface GetsByFilterParams {
+    filter: object;
+    sorts: object;
+    options: Options;
+}
+
+// * get documents by filter of collection
+// * @param {object} _params - params object
+// * @param {object} _params.filter - filter of attrs.
+// * @param {object} _params.like - the object to like the documents to get from the collection
+// * @param {array} _params.sorts - array of sort str for attrs.
+// * @param {object} _params.options - query options, e.g. keepAttrs-res attrs; hasEdge-edge res;
+// * @return {AqlQuery} - interface AqlQuery by arangodb
+
+export let GetsByFilterParamsSchema = Joi.object({
+    filter: Joi.object().required(),
+    sorts: Joi.object(),
+    options: Joi.object(),
+}).required();
+
+export interface UpdatesByFilterParams {
+    filter: object;
+    newObj: object;
+    options: Options;
+}
+
+export let UpdatesByFilterParamsSchema = Joi.object({
+    filter: Joi.object().required(),
+    newObj: Joi.object().required(),
+    options: Joi.object(),
+}).required();
+
+export interface SaveByFilterParams {
+    obj: object;
+    options: Options;
+}
+
+export let SaveByFilterParamsSchema = Joi.object({
+    obj: filterMinRequired.required(),
+    options: Joi.object(),
+}).required();
+
+export interface DeletesByFilterParams {
+    filter: object;
+    options: Options;
+}
+
+export let DeletesByFilterParamsSchema = Joi.object({
+    filter: Joi.object().required(),
+    options: Joi.object(),
+}).required();
+
+export interface executeProcedureParams {
+    json: string;
+}
+
+export let executeProcedureParamsSchema = Joi.object({
+    json: Joi.string().required(),
 }).required();
 
 export interface ExecuteSqlParams {
     //todo 增加对象的具体字段
     sql: string;
-    binds?: object;
+    binds?: object | Array<any>;
     options?: object;
 }
 
 export let ExecuteSqlParamsSchema = Joi.object({
     sql: Joi.string().required(),
-    binds: Joi.object(),
+    binds: Joi.alternatives().try(Joi.object(), Joi.array()),
     options: Joi.object(),
 }).required();
